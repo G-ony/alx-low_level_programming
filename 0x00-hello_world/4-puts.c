39cd7e4e2120d31dfc5940eec12fdf5a96e71ce0
G-ony <johnwanjala1997@gmail.com>
2022-12-08 12:26:55 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,19 @@
 #include <stdio.h>
 
-
-
 /**
  *
- *  * main - Entry point
+ *  * Description: main - Prints out a string to stdout.
  *
- *   * Description: prints "\"Programming is like building a multilingual puzzle"
+ *   * Return: 0 if success.
  *
- *    * Return: 0
- *
- *     */
-
-
+ *    */
 
 int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+	return (0);
 
 }
