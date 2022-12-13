32cd66cbe2381509d1336d7f4ebcc20a1b4823be
G-ony <johnwanjala1997@gmail.com>
2022-12-13 10:15:27 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,7 +14,7 @@ int print_sign(int n)
 
 	if (n > 0)
 
-				{
+	{
 
 		_putchar('+');
 		return (1);
@@ -32,6 +32,6 @@ int print_sign(int n)
 	_putchar('-');
 	}
 
-		return (-1);
+	return (-1);
 
 }
