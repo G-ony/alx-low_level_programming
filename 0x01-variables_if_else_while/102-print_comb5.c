76cb1f288d22163379a2d39aef98f4dc4251f03b
G-ony <johnwanjala1997@gmail.com>
2022-12-13 06:10:43 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,25 +18,24 @@ int main(void)
 		for (y = x + 1 ; y < max; y++)
 		{
 
+		putchar (x / 10 + '0');
+		putchar (x % 10 + '0');
+		putchar (' ');
 
-	putchar (x / 10 + '0');
-	putchar (x % 10 + '0');
-	putchar (' ');
+		putchar (y / 10 + '0');
+		putchar (y % 10 + '0');
+	
+		if (x == 98 && y == 99)
+			break;
 
-	putchar (y / 10 + '0');
-	putchar (y % 10 + '0');
+		putchar(',');
+		putchar (' ');
 
-	if (x == 98 && y == 99)
-		break;
-
-	putchar(',');
-	putchar (' ');
-
+		}
+	
 	}
 
 	putchar('\n');
 
 	return (0);
-
-	}
 }
