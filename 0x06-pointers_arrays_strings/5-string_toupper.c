5ac3db07c12840d535f11e79879c8f498d320ee4
John Wanjala <43532333+G-ony@users.noreply.github.com>
2022-12-21 15:54:46 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,41 +1,21 @@
 #include "main.h"
-
 /**
+ * string_toupper - change lowercase letters to uppercase.
+ * @s: analized string.
  *
- *  * string_toupper - change lowercase letters to uppercase.
- *
- *   * @s: analized string.
- *
- *    *
- *
- *     * Return: String with all letters Uppercased.
- *
- *      */
-
+ * Return: String with all letters Uppercased.
+ */
 char *string_toupper(char *s)
-
 {
+	int i = 0;
 
-		int i = 0;
-
-
-
-			while (*(s + i) != '\0')
-
-					{
-
-								if (*(s + i) >= 'a' && *(s + i) <= 'z')
-
-											{
-
-															*(s + i) = *(s + i) - 32;
-
-																	}
-
-										i++;
-
-											}
-
-				return (s);
-
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			*(s + i) = *(s + i) - 32;
+		}
+		i++;
+	}
+	return (s);
 }
