a1c7c57281e28bcdf718b5fa9b477938a74c5435
John Wanjala <43532333+G-ony@users.noreply.github.com>
2022-12-21 15:42:34 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,41 +5,22 @@
  * @src: string to be concatenated
  * Return: Always 0.
  */
-
 char *_strcat(char *dest, char *src)
-
 {
+	int lengthD, lengthS;
 
-		int lengthD, lengthS;
+	lengthD = 0;
+	lengthS = 0;
 
+	while (*(dest + lengthD) != '\0')
+		lengthD++;
 
-
-			lengthD = 0;
-
-				lengthS = 0;
-
-
-
-					while (*(dest + lengthD) != '\0')
-
-								lengthD++;
-
-
-
-						while (*(src + lengthS) != '\0' && lengthD < 97)
-
-								{
-
-											*(dest + lengthD) = *(src + lengthS);
-
-													lengthD++;
-
-															lengthS++;
-
-																}
-
-							*(dest + lengthD) = '\0';
-
-								return (dest);
-
+	while (*(src + lengthS) != '\0' && lengthD < 97)
+	{
+		*(dest + lengthD) = *(src + lengthS);
+		lengthD++;
+		lengthS++;
+	}
+	*(dest + lengthD) = '\0';
+	return (dest);
 }
